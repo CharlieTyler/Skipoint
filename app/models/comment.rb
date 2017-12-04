461802611ceed9dce317d4aee752e4ec54fff99b
Charlie Tyler <charlietyler@live.com>
2017-12-04 15:21:49 +0000

    Added comments to places page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    '1 Star': '1_star',
+    '2 Stars': '2_star',
+    '3 Stars': '3_star',
+    '4 Stars': '4_star',
+    '5 Stars': '5_star'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
